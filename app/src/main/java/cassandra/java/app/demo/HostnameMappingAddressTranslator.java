de5bb86b7a11f0a92144489713c21803829fae68
Arvydas Jonusonis <arvydas@jonusonis.com>
2022-09-16 11:53:55 -0700

    ..

--- app/src/main/java/cassandra/java/app/demo/HostnameMappingAddressTranslator.java
+++ app/src/main/java/cassandra/java/app/demo/HostnameMappingAddressTranslator.java
@@ -1,6 +1,8 @@
 package cassandra.java.app.demo;
 
 import com.datastax.oss.driver.api.core.addresstranslation.AddressTranslator;
+import com.datastax.oss.driver.api.core.config.DriverOption;
+import com.datastax.oss.driver.api.core.context.DriverContext;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
 import java.net.InetAddress;
@@ -9,7 +11,7 @@ import java.net.UnknownHostException;
 
 public class HostnameMappingAddressTranslator implements AddressTranslator {
 
-    public HostnameMappingAddressTranslator() {
+    public HostnameMappingAddressTranslator(DriverContext context, DriverOption configRoot) {
     }
 
     @NonNull
@@ -17,9 +19,9 @@ public class HostnameMappingAddressTranslator implements AddressTranslator {
     public InetSocketAddress translate(@NonNull InetSocketAddress address) {
         try {
             if( address.getAddress().equals( InetAddress.getByAddress(new byte[]{10,101,33,(byte)222}) ) ){
-                return InetSocketAddress.createUnresolved("ip-10-101-33-222.srv101.dsinternal.org", 9042);
+                return InetSocketAddress.createUnresolved("example.com", 9042);
             } else if (address.getAddress().equals( InetAddress.getByAddress(new byte[]{10, 101, 34, (byte)187}) ) ){
-                return InetSocketAddress.createUnresolved("ip-10-101-34-187.srv101.dsinternal.org", 9042);
+                return InetSocketAddress.createUnresolved("example.com", 9042);
             }
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
